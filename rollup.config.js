3237c24b4fd088b792c4c80dd409dfef3268cf86
Vincent Barrault <vincentb45@gmail.com>
2021-09-11 23:54:10 +0200

    fix: change typescript plugin, add rimraf (#3)
    * fix: change typescript plugin, add rimraf
    
    * fix: add missing stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
