c78763b794aa790754699803d11965a18a2a0971
Vincent <vincentb45@gmail.com>
2021-09-22 17:36:39 +0200

    fix: remove debug changes

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import config from './config';
 
 var observer = new MutationObserver((mutations) => {
     // TODO: optimize this part.
-    // console.log(mutations);
-    mutations.forEach(mutation => console.log(mutation.addedNodes));
     pixairize(config);
 });
 
