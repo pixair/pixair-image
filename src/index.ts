cf84d02b87e9edaa28c83e6683bded8df0b6b546
Vincent Barrault <vincentb45@gmail.com>
2021-10-10 23:28:24 +0200

    feat(mutation): optimization mutation based on img (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { pixairize } from "./pixairize";
+import { pixairize } from './pixairize';
 import config from './config';
 
 var observer = new MutationObserver((mutations) => {
-    // TODO: optimize this part.
-    pixairize(config);
+    let needUpdate = false;
+
+    mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+            const html = (node as HTMLElement).innerHTML;
+            if (!html) return;
+
+            if (html.indexOf('img') != -1) {
+                needUpdate = true;
+            }
+        })
+    });
+
+    if (needUpdate) {
+        pixairize(config);
+    }
 });
 
 observer.observe(
     document.documentElement || document.body,
-    { attributes: true, childList: true, subtree: true }
+    { childList: true, subtree: true }
 );
